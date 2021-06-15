117cdc5a3e762e58c0e169dcd9d52d520855fb4c
unknown <samson_knight@yahoo.com>
2021-06-15 19:10:56 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TitleBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
